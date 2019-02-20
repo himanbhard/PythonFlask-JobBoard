68fc0ff74f8213def53188e312b146ad43ba2c10
Himanshu Bhardwaj <himanbhard@gmail.com>
2019-02-20 10:20:29 -0500

    Update app.py
    added sql lite configs

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,19 @@
-from flask import Flask,render_template, url_for
+import sqllite3
+from flask import Flask,render_template, g, url_for
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+
+def open_connection():
+        connection = getattr(g,'_connection', None)
+        if connection == 'None'
+            connection = g.connection=sqllite3.connect(PATH)
+        connection.row_factory= sqllite3.Row
+        return connection
+
+def execute_sql(sql,values=(),commit= False,single= False):
+    connection = open_connection()
+
 
 @app.route("/")
 @app.route("/jobs")
