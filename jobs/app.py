080d5ecaa0c611113bf5621644438565526f33fa
Himanshu Bhardwaj <himanbhard@gmail.com>
2019-02-20 10:28:32 -0500

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -11,8 +11,23 @@ def open_connection():
         connection.row_factory= sqllite3.Row
         return connection
 
-def execute_sql(sql,values=(),commit= False,single= False):
+def execute_sql(sql, values=(),commit=False, single=False):
     connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit = True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
 
 
 @app.route("/")
